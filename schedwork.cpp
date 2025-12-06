b119346ca97d60bee810d26c9ba0cc1a04c56e66
Isaac Lee <ihlee@usc.edu>
2025-12-06 06:24:02 +0000

    done!

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,26 +20,68 @@ using namespace std;
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
 
+bool sched_helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t>& worker_shift_cnt, size_t loc);
+bool find_if_worker_already_scheduled(const DailySchedule& sched, int day, int w, int curr_slot);
 
-// Add your implementation of schedule() and other helper functions here
-
-bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
-)
+bool schedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched)
 {
-    if(avail.size() == 0U){
-        return false;
-    }
-    sched.clear();
-    // Add your code below
+  if(avail.size() == 0U){
+      return false;
+  }
+  sched.clear();
+  size_t num_days = avail.size();
+  sched.resize(num_days);
+  for(int i = 0; i < avail.size(); i++){
+    sched[i].resize(dailyNeed);
+  }
 
-
-
-
+  size_t num_workers = avail[0].size();
+  vector<size_t> worker_shift_cnt(num_workers, 0);
+  
+  return sched_helper(avail, dailyNeed, maxShifts, sched, worker_shift_cnt, 0);
 }
+
+bool sched_helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t>& worker_shift_cnt, size_t loc){
+  int num_days = avail.size();
+  int num_workers = avail[0].size();
+  size_t total_loc = num_days * dailyNeed;
+
+  if(loc == total_loc){
+    return true;
+  }
+
+  int day = loc/dailyNeed;
+  int slt = loc%dailyNeed;
+  for(int w = 0; w < num_workers; w++){
+    if(worker_shift_cnt[w] >= maxShifts){
+      continue;
+    }
+    else if(avail[day][w] == false){
+      continue;
+    }
+    else if(find_if_worker_already_scheduled(sched, day, w, slt)){
+      continue;
+    }
+    sched[day][slt] = w;
+    worker_shift_cnt[w]++;
+
+    if(sched_helper(avail, dailyNeed, maxShifts, sched, worker_shift_cnt, loc+1)){
+      return true;
+    }
+    worker_shift_cnt[w]--;
+  }
+  return false;
+}
+
+bool find_if_worker_already_scheduled(const DailySchedule& sched, int day, int w, int curr_slot){
+  for(int i = 0; i < curr_slot; i++){
+    if(sched[day][i] == w){
+      return true;
+    }
+  }
+  return false;
+}
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
-
+void make_words(string curr, int loc, const string& floating, const set<string>& dict, set<string>& res);
+bool all_are_floating_letters(const string& word, const string& floating);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +21,45 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    set<string> res;
+    make_words(in, 0, floating, dict, res);
+    return res;
 }
 
-// Define any helper functions here
+void make_words(string curr, int loc, const string& floating, const set<string>& dict, set<string>& res){
+    // base
+    if(loc == curr.size()){
+        if(all_are_floating_letters(curr, floating) == true){
+            if(dict.find(curr) != dict.end()){
+                res.insert(curr);
+            } 
+        }
+        return;
+    }
+
+    if(curr[loc] != '-'){
+        make_words(curr, loc+1, floating, dict, res);
+    }
+    else{
+        for(char c = 'a'; c <= 'z'; c++){
+            curr[loc] = c;
+            make_words(curr, loc+1, floating, dict, res);
+        }
+    }
+}
+
+bool all_are_floating_letters(const string& word, const string& floating){
+    for(int x = 0; x < floating.length(); x++){
+        bool we_found_it = false;
+        for(int y = 0; y < word.length(); y++){
+            if(floating[x] == word[y]){
+                we_found_it = true;
+                break;
+            }
+        }
+        if(we_found_it == false){
+            return false;
+        }
+    }
+    return true;
+}
